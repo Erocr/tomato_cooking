ebbbc8ae8d51ab7542ceb06523a54912cc8517e5
Niccolo Angeli <niccolo.angeli@universite-paris-saclay.fr>
2025-09-25 22:07:53 +0200

    add: resize

--- View.py
+++ View.py
@@ -1,11 +1,28 @@
 import pygame as pg
 from Vec import *
+from os import listdir
 
 
 class View:
     def __init__(self):
         self.factor = Vec(600, 600) / Vec(1920, 1080)
         self.screen = pg.display.set_mode((Vec(1920, 1080) * self.factor).get(), pg.RESIZABLE)
+        self.sprites = {}
+        self.standard_scale_sprites = {}
+        self.load_sprites()
+        self.resize_sprites()
+
+    def load_sprites(self):
+        for file in listdir("sprites/"):
+            im = pg.image.load("sprites/"+file)
+            self.standard_scale_sprites[file.split(".")[0]] = im
+
+    def resize_sprites(self):
+        for sprite in self.standard_scale_sprites:
+            self.sprites[sprite] = pg.transform.scale_by(self.standard_scale_sprites[sprite], self.factor.get())
+
+    def draw_sprite(self, image, position):
+        self.screen.blit(self.sprites[image], self.game2screen(position).get())
 
     def game2screen(self, coord):
         return coord * self.factor
@@ -18,6 +35,7 @@ class View:
         if window_resized:
             new_size = Vec(*pg.display.get_window_size())
             self.factor = new_size / Vec(1920, 1080)
+            self.resize_sprites()
 
     def draw_circle(self, center, radius, color=(0, 0, 0)):
         pg.draw.circle(self.screen, color, center.get(), radius)
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Inputs import Inputs
 from View import View
+from Vec import *
 
 view = View()
 inputs = Inputs()
@@ -8,4 +9,7 @@ inputs = Inputs()
 while not inputs.quit:
     inputs.update()
 
+    view.draw_sprite("tomatest", Vec(1920/2, 1080/2))
+
     view.update(inputs.get_resized())
+    view.fill()
